64ed1b03aaba6eebc983337b0079727b2ae128db
SergioFoma <s.kuzminsky.2007@gmail.com>
2025-10-29 03:25:10 +0300

    correct insert

--- src/list.cpp
+++ src/list.cpp
@@ -18,7 +18,7 @@ listErrors initList( List* myList ){
     CHECK_PTR( myList->next, PREV_INITIALIZATION_ERR );
 
     myList->freeIndex = 1;
-    myList->headIndex = 1;
+    myList->headIndex = 0;
     myList->tailIndex = 0;
     myList->data[0] = canary;
     myList->data[ startSizeForArray - 1 ] = canary;
@@ -58,14 +58,21 @@ size_t listInsert( List* myList, double number , size_t indexToPush ){
 
     myList->data[ freeIndexNow ] = number;
 
+    // ARRAY NEXT
+    myList->next[ freeIndexNow ] = myList->next[ indexToPush ];
+    if( indexToPush != 0 ){
+        myList->next[ indexToPush ] = freeIndexNow;
+    }
+    else if( freeIndexNow != 1 ){
+        myList->next[ freeIndexNow ] = myList->headIndex;
+    }
+
     // ARRAY PREV
-    if( indexToPush >= myList->tailIndex && indexToPush != 0 ){
+    /*if( indexToPush >= myList->tailIndex && indexToPush != 0 ){
         myList->prev[ freeIndexNow ] = myList->tailIndex;
-        //myList->prev[ myList->prev ] = freeIndexNow;
     }
     else if( freeIndexNow != 1 && indexToPush == 0 ){
         myList->prev[ freeIndexNow ] = myList->prev[ myList->headIndex ];
-        printf("\n__________head Index_______ %lu\n", myList->headIndex );
         myList->prev[ myList->headIndex ] = freeIndexNow;
     }
     else if( freeIndexNow == 1 && indexToPush == 0 ){
@@ -74,21 +81,35 @@ size_t listInsert( List* myList, double number , size_t indexToPush ){
     else{
         myList->prev[ freeIndexNow ] = indexToPush;
         myList->prev[ indexToPush + 1 ] = freeIndexNow;
+    }*/
+
+    myList->prev[ freeIndexNow ] = indexToPush;
+    if( indexToPush != 0 && myList->next[ freeIndexNow ] != 0 ){
+        myList->prev[ myList->next[ freeIndexNow ] ] = freeIndexNow;
+    }
+    else if( freeIndexNow != 1 && indexToPush == 0 ){
+        myList->prev[ freeIndexNow ] = myList->prev[ myList->headIndex ];
+        myList->prev[ myList->headIndex ] = freeIndexNow;
     }
 
-    // ARRAY NEXT
-    myList->next[ freeIndexNow ] = myList->next[ indexToPush ];
-    if( indexToPush != 0 ){
-        myList->next[ indexToPush ] = freeIndexNow;
-    }
-    else if( freeIndexNow != 1 ){
-        myList->next[ freeIndexNow ] = myList->headIndex;
+
+    /*else if( myList->next[ freeIndexNow ] != 0 ){
+        myList->prev[ freeIndexNow ] = myList->prev[ myList->next[ freeIndexNow ] ];
+    }*/
+    /*else{
+        //printf( "\nprint in else: myList->next[ freeIndexNow ] = %d\n", myList->next[ freeIndexNow ] );
+        myList->prev[ freeIndexNow ] = indexToPush;
+    }*/
+
+    // headIndex && tailIndex
+
+    if( indexToPush == 0){
         myList->headIndex = freeIndexNow;
     }
-
-    if( myList->tailIndex <= indexToPush ){
+    if( myList->next[ freeIndexNow ] == 0 ){
         myList->tailIndex = freeIndexNow;
     }
+
     printf("\nindexTOPush = %lu\n", indexToPush );
     printList( myList );
     return freeIndexNow;
